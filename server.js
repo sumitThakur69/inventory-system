8cbbefa3f270ef8e24bdedeb69500dcde903b825
Sumit Thakur <samtak190@gmail.com>
2025-06-19 20:11:02 +0530

    just test the git working or not

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const localhost = `http://localhost:${PORT}`;
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+app.get('/api', (req, res) => {
+  res.json({ message: 'API is working!' });
+});
 
 
 app.listen(process.env.PORT || 3000 , () => {
